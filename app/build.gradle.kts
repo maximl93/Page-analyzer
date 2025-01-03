3721aff406671ec728e2229784a74b9391fe6009
Maksim Lukin <maximlm93@gmail.com>
2025-01-03 16:44:33 +0500

    project init

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    checkstyle
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
