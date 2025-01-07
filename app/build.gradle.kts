1ba9eb0a2a0a36232f1e2d36f76916bd8b66a8b7
Maksim Lukin <maximlm93@gmail.com>
2025-01-07 16:37:46 +0500

    add postgresql driver dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ application {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql:42.7.4")
     implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.1.0")
 
